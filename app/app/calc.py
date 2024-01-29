d0e75e6d97ef8afb01b6b6adaba1009473a7e89f
cvsingh <cvsingh9888@gmail.com>
2024-01-29 16:13:10 +0530

    sh -c "python manage.py wait_for_db &&

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculate functions
+"""
+
+
+def add(x, y):
+    """Add x and y"""
+    return x + y
+
+
+def subtract(x, y):
+    """ Substracting y from x """
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+Simple test case
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """ Test the calc module"""
+    def test_add_numbers(self):
+        """Test adding numbers"""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """ Test substracting numbers """
+        res = calc.subtract(10, 4)
+
+        self.assertEqual(res, 6)
